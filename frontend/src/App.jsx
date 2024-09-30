0605910079b1614f5fbd40490f89919be2ebe735
Tiago da Silva Barbosa <tiagoproedu@gmail.com>
2024-09-30 16:36:49 -0300

    vinculando front e back

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
 
-  const [todo, setTodo] = useState([]);
+  /*const [todo, setTodo] = useState([]);
   const todoRef = useRef(null);
 
   function addTodo() {
@@ -10,21 +10,61 @@ function App() {
     setTodo(add);
   }
   function todoList(to, i) {
-    return(<div key={i}><input type="checkbox" id={to}/><label for={to}>{to}</label></div>)
+    return (<div key={i}><input type="checkbox" id={to} /><label for={to}>{to}</label></div>)
+  }*/
+
+  const [todo, setTodo] = useState([]);
+  const todoRef = useRef(null);
+
+  function addTodo() {
+    const newTodo = todoRef.current.value;
+    setTodo([...todo, newTodo]);
+
+    fetch('http://localhost:8080/todo', {
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ tarefa: newTodo })
+    })
+    .then(response => {
+      if(!response.ok){
+        throw new Error('Erro ao adicionar tarefa');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Tarefa adicionada com sucesso: ', data);
+    })
+    .catch(error => {
+      console.error('Erro: ', error);
+    })
   }
 
-  return(
-    <>
-    <h1>TO-DO LIST</h1>
+  useEffect(() => {
+    fetch('http://localhost:8080/todo')
+    .then(response => response.json())
+    .then(data => setTodo(data));
+  }, []);
 
-    <div className="toDo">
-      <input type="text" ref={todoRef}/>
-      <button onClick={addTodo}>Adicionar</button>
-      <fieldset>
-        <legend>Tarefas:</legend>
-        {todo.map(todoList)}
-      </fieldset>
-    </div>
+  function todoList(task, index){
+    return (<div key={index}><input type="checkbox" id={task}/><label for={task}>{task}</label></div>);
+  }
+  
+  return (
+    <>
+      <h1>TO-DO LIST</h1>
+
+      <div className="toDo">
+        <input type="text" ref={todoRef} />
+        <button onClick={addTodo}>Adicionar</button>
+        <fieldset>
+          <legend>Tarefas:</legend>
+          <ul>
+            {todo.map(todoList)}
+          </ul>
+        </fieldset>
+      </div>
     </>
   );
 }
